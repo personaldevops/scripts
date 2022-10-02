cbcbd8e41a49df2e8d07c42bd2e5a0f304f30a53
Naveen Raj B <naveenpro111@gmail.com>
2022-10-02 13:17:10 +0530

    Script to clone all repos

---
+++ clone_all.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:personaldevops/configs.git
+
+git clone git@github.com:personaldevops/scripts.git
+
+git clone git@github.com:personaldevops/abstraction.git
+
+git clone git@github.com:personaldevops/api_servicecore.git
+
+git clone git@github.com:personaldevops/appservice.git
+
+git clone git@github.com:personaldevops/authservice.git
