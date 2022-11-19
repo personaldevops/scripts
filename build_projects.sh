a2eee003a589aa167c3bdf4dae54104c0c084678
Naveen Raj B <naveenpro111@gmail.com>
2022-11-20 01:41:50 +0530

    Added list project status with modified and untracked count along with branch

--- build_projects.sh
+++ build_projects.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
+c#!/usr/bin/env bash
 SCRIPTDIR=$(dirname "$0")
 BASEDIR=$(cd "$SCRIPTDIR" && pwd)
 HOMEDIR=${BASEDIR}/..
 cd "${HOMEDIR}/abstraction"
-pip install -r requirements.txt .
+pip3 install -r requirements.txt .
 cd ..
 cd "${HOMEDIR}/api_servicecore"
-pip install -r requirements.txt .
+pip3 install -r requirements.txt .
 cd ..
 cd "${HOMEDIR}/appservice"
-pip install -r requirements.txt .
+pip3 install -r requirements.txt .
 cd ..
 cd "${HOMEDIR}/authservice"
-pip install -r requirements.txt .
+pip3 install -r requirements.txt .
+python -m pip install autopep8
 
---
+++ list_status_projects.sh
@@ -0,0 +1,36 @@
+repos=(
+"abstraction"
+"api_servicecore"
+"appservice"
+"authservice"
+"configs"
+"scripts"
+)
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+printf "| %20s %30s %30s %40s |\n" "Repository" "Modified" "Untracked" "Branch"
+echo "-------------------------------------------------------------------------------------------------------------------------------"
+for i in "${repos[@]}"
+do
+  cd $CODEBASE_DIR/$i
+  changes='yes'
+  branch=$(git branch)
+  mods=$(git ls-files --modified)
+  unt=$(git ls-files --others --exclude-standard)
+  mods=(${mods//"\n"/ })
+  mods=${#mods[@]}
+  unt=(${unt//"\n"/ })
+  unt=${#unt[@]}
+  if [[ $mods != 0 ]]
+  then
+    printf "| \033[0;31m%20s %27s %30s %43s \033[0m|\n" $i $mods $unt ${branch:2}
+  elif [[ $mods == 0 && $unt != 0 ]]
+  then
+    printf "| \033[1;33m%20s %27s %30s %43s \033[0m|\n" $i $mods $unt ${branch:2}
+  else
+    printf "| %20s %27s %30s %43s |\n" $i $mods $unt ${branch:2}
+  fi
+done
+echo "-------------------------------------------------------------------------------------------------------------------------------"
