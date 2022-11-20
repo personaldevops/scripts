98f3f69d7aec738679fc112f36446e0c517c043c
Naveen Raj B <naveenpro111@gmail.com>
2022-11-20 14:19:12 +0530

    Changes to list newly added repositories in the codebase folder automatically with option to exclude folders

--- list_status_projects.sh
+++ list_status_projects.sh
@@ -1,17 +1,23 @@
-repos=(
-"abstraction"
-"api_servicecore"
-"appservice"
-"authservice"
-"configs"
-"scripts"
-)
+cd $CODEBASE_DIR
+repos=$(ls -d */)
+repos=(${repos//" "/ })
+exclude=("notebooks")
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 echo "-------------------------------------------------------------------------------------------------------------------------------"
 printf "| %20s %30s %30s %40s |\n" "Repository" "Modified" "Untracked" "Branch"
 echo "-------------------------------------------------------------------------------------------------------------------------------"
+for i in "${!repos[@]}"
+do
+  for j in "${exclude[@]}"
+  do
+    if [[ ${repos[i]::-1} == $j ]]
+    then
+      unset 'repos[i]'
+    fi
+  done
+done
 for i in "${repos[@]}"
 do
   cd $CODEBASE_DIR/$i
@@ -25,12 +31,12 @@ do
   unt=${#unt[@]}
   if [[ $mods != 0 ]]
   then
-    printf "| \033[0;31m%20s %27s %30s %43s \033[0m|\n" $i $mods $unt ${branch:2}
+    printf "| \033[0;31m%20s %27s %30s %43s \033[0m|\n" ${i::-1} $mods $unt ${branch:2}
   elif [[ $mods == 0 && $unt != 0 ]]
   then
-    printf "| \033[1;33m%20s %27s %30s %43s \033[0m|\n" $i $mods $unt ${branch:2}
+    printf "| \033[1;33m%20s %27s %30s %43s \033[0m|\n" ${i::-1} $mods $unt ${branch:2}
   else
-    printf "| %20s %27s %30s %43s |\n" $i $mods $unt ${branch:2}
+    printf "| %20s %27s %30s %43s |\n" ${i::-1} $mods $unt ${branch:2}
   fi
 done
 echo "-------------------------------------------------------------------------------------------------------------------------------"
